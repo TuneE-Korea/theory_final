792358b0237fb5e9700ece3fbc3e1db0ba266464
김태윤 <xodbs1758@gmail.com>
2025-08-21 16:41:02 +0900

    08/21 first

--- app/state/page.jsx
+++ app/state/page.jsx
@@ -7,35 +7,30 @@ const Page = () => {
   const addTodo = () => {
     setArr((prev) => [...prev, input]);
   };
-  const removeTodo = (idx) => {
-    setArr((prev) => {
-      delete prev[idx];
-      return [...prev];
-    });
-  };
   return (
     <>
       <div className="flex gap-2">
         <input
           onChange={(e) => setInput((prev) => e.target.value)}
-          className="bg-sky-200"
+          className="bg-sky-500"
           placeholder="오늘 할일 입력"
           type="text"
         />
-        <button onClick={addTodo} className="bg-red-100 rounded-full p-1">
+        <button onClick={addTodo} className="bg-red-500 rounded-full p-1">
           등록
         </button>
       </div>
       <div>
         <span>오늘 해야할 일</span>
-        <div className="bg-amber-100 w-fit flex flex-col">
-          {arr.map((x, i) => (
+        <div className="bg-amber-500 w-fit flex flex-col">
+          {arr.map((v, i) => (
             <div key={i} className="flex gap-2 items-center justify-between">
-              <span>{x}</span>
+              <span>{v}</span>
               <Trash2
                 className="cursor-pointer text-red-500"
-                // filter 써야함
-                onClick={() => removeTodo(i)}
+                onClick={() =>
+                  setArr((prev) => [...prev].filter((x) => x != v))
+                }
               />
             </div>
           ))}
@@ -47,7 +42,9 @@ const Page = () => {
 
 export default Page;
 // 스테이트를 몇 개 선언해야할지 설계하고 들어가야할듯
-// 등록이 될때마다 줄바뀜이 되도록 ...?
+// 등록이 될때마다 줄바뀜이 되도록 ...? -> 해결 완료
 // ㄴ 배열로 어떻게 만들지? 줄바꿈 문자를 누적하는 방식은 별로 좋지 않아 보인다.
 // 지금 코드 결과는 등록을 누를때마다 값이 초기화되지만, 기존의 입력들이 유지되도록 ...? -> 해결 완료
 // 리액트 리렌더링 원칙 공부하기 !!
+// ㄴ 기본적으로,
+// filter 사용법 복습
